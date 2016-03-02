084f775b043223ccceabf38f5a40fefc5ee55c0b
Sotaya <sotayamy@gmail.com>
2016-03-02 18:02:21 +0100

    Splash screen animation

--- Monitor/app/build/generated/source/r/debug/com/Server/camerapreview/R.java
+++ Monitor/app/build/generated/source/r/debug/com/Server/camerapreview/R.java
@@ -8,12 +8,16 @@
 package com.Server.camerapreview;
 
 public final class R {
+    public static final class anim {
+        public static final int alpha=0x7f040000;
+        public static final int translate=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f050000;
-        public static final int blue=0x7f050001;
-        public static final int white=0x7f050002;
+        public static final int black=0x7f060000;
+        public static final int blue=0x7f060001;
+        public static final int white=0x7f060002;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
@@ -27,26 +31,27 @@ public final class R {
         public static final int icon=0x7f020008;
     }
     public static final class id {
-        public static final int button1=0x7f080012;
-        public static final int checkBox1=0x7f08000d;
-        public static final int checkBox2=0x7f08000e;
-        public static final int editText1=0x7f08000a;
-        public static final int editText2=0x7f08000b;
-        public static final int editText3=0x7f08000f;
-        public static final int editText4=0x7f08000c;
-        public static final int imageButton1=0x7f080001;
-        public static final int imageButton2=0x7f080000;
-        public static final int imageButton3=0x7f080005;
-        public static final int imageButton4=0x7f080007;
-        public static final int imageView1=0x7f080010;
-        public static final int menu_settings=0x7f080013;
-        public static final int scrollView1=0x7f080009;
-        public static final int surfaceView=0x7f080011;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080003;
-        public static final int textView3=0x7f080002;
-        public static final int textView4=0x7f080006;
-        public static final int textView5=0x7f080008;
+        public static final int button1=0x7f090013;
+        public static final int checkBox1=0x7f09000d;
+        public static final int checkBox2=0x7f09000e;
+        public static final int editText1=0x7f09000a;
+        public static final int editText2=0x7f09000b;
+        public static final int editText3=0x7f09000f;
+        public static final int editText4=0x7f09000c;
+        public static final int imageButton1=0x7f090001;
+        public static final int imageButton2=0x7f090000;
+        public static final int imageButton3=0x7f090005;
+        public static final int imageButton4=0x7f090007;
+        public static final int imageView1=0x7f090011;
+        public static final int lin_lay=0x7f090010;
+        public static final int menu_settings=0x7f090014;
+        public static final int scrollView1=0x7f090009;
+        public static final int surfaceView=0x7f090012;
+        public static final int textView1=0x7f090004;
+        public static final int textView2=0x7f090003;
+        public static final int textView3=0x7f090002;
+        public static final int textView4=0x7f090006;
+        public static final int textView5=0x7f090008;
     }
     public static final class layout {
         public static final int activity_list_item=0x7f030000;
@@ -56,40 +61,40 @@ public final class R {
         public static final int preview=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
-        public static final int activity_review=0x7f070001;
-        public static final int activity_settings=0x7f070002;
-        public static final int activity_splash=0x7f070003;
-        public static final int preview=0x7f070004;
+        public static final int activity_main=0x7f080000;
+        public static final int activity_review=0x7f080001;
+        public static final int activity_settings=0x7f080002;
+        public static final int activity_splash=0x7f080003;
+        public static final int preview=0x7f080004;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int capture=0x7f060001;
-        public static final int delay=0x7f060002;
-        public static final int description=0x7f060003;
-        public static final int email=0x7f060004;
-        public static final int email_check=0x7f060005;
-        public static final int face=0x7f060006;
-        public static final int icon_description=0x7f060007;
-        public static final int intro=0x7f060008;
-        public static final int log=0x7f060009;
-        public static final int menu=0x7f06000a;
-        public static final int menu_settings=0x7f06000b;
-        public static final int password=0x7f06000c;
-        public static final int phone=0x7f06000d;
-        public static final int phone_check=0x7f06000e;
-        public static final int preview=0x7f06000f;
-        public static final int preview_icon_text=0x7f060010;
-        public static final int review=0x7f060011;
-        public static final int review_icon_text=0x7f060012;
-        public static final int service=0x7f060013;
-        public static final int settings=0x7f060014;
-        public static final int title_activity_main=0x7f060015;
-        public static final int title_activity_review=0x7f060016;
-        public static final int title_activity_settings=0x7f060017;
-        public static final int title_activity_splash=0x7f060018;
-        public static final int version=0x7f060019;
-        public static final int viewer=0x7f06001a;
+        public static final int app_name=0x7f070000;
+        public static final int capture=0x7f070001;
+        public static final int delay=0x7f070002;
+        public static final int description=0x7f070003;
+        public static final int email=0x7f070004;
+        public static final int email_check=0x7f070005;
+        public static final int face=0x7f070006;
+        public static final int icon_description=0x7f070007;
+        public static final int intro=0x7f070008;
+        public static final int log=0x7f070009;
+        public static final int menu=0x7f07000a;
+        public static final int menu_settings=0x7f07000b;
+        public static final int password=0x7f07000c;
+        public static final int phone=0x7f07000d;
+        public static final int phone_check=0x7f07000e;
+        public static final int preview=0x7f07000f;
+        public static final int preview_icon_text=0x7f070010;
+        public static final int review=0x7f070011;
+        public static final int review_icon_text=0x7f070012;
+        public static final int service=0x7f070013;
+        public static final int settings=0x7f070014;
+        public static final int title_activity_main=0x7f070015;
+        public static final int title_activity_review=0x7f070016;
+        public static final int title_activity_settings=0x7f070017;
+        public static final int title_activity_splash=0x7f070018;
+        public static final int version=0x7f070019;
+        public static final int viewer=0x7f07001a;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
@@ -100,9 +105,9 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
--- Monitor/app/src/main/java/activities/SplashActivity.java
+++ Monitor/app/src/main/java/activities/SplashActivity.java
@@ -5,32 +5,85 @@ import java.util.TimerTask;
 
 import com.Server.camerapreview.R;
 
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import static com.Server.camerapreview.R.anim.translate;
 
 public class SplashActivity extends Activity {
 
 	private int delay = 1500;
+
+	public void onAttachedToWindow(){
+		super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+	}
+
+    Thread splashThread;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
-		
-		TimerTask task = new TimerTask(){
-
-			@Override
-			public void run() {
-				finish();
-				Intent mainIntent = new Intent().setClass(SplashActivity.this, 
-						MainActivity.class);
-				startActivity(mainIntent);
-			}
-        	
-        };
-        
-        Timer timer = new Timer();
-        timer.schedule(task, delay);
+		startAnimation();
+//		TimerTask task = new TimerTask(){
+//
+//			@Override
+//			public void run() {
+//				finish();
+//				Intent mainIntent = new Intent().setClass(SplashActivity.this,
+//						MainActivity.class);
+//				startActivity(mainIntent);
+//			}
+//
+//        };
+//
+//        Timer timer = new Timer();
+//        timer.schedule(task, delay);
 	}
+
+    private void startAnimation() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (waited < 3500){
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+        splashThread.start();
+    }
 
 }
